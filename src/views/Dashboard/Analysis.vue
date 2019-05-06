dac591076e8e275adb07a3c36aa633e17e268797
thewangzl <thewangzl@gmail.com>
2019-05-06 22:15:29 +0800

    与服务端进行交互（Axios）

--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import axios from "axios";
+import request from "../../utils/request";
 import Chart from "../../components/Chart";
 import { setInterval } from "timers";
 export default {
@@ -22,27 +22,29 @@ export default {
   },
   methods: {
     getChartData() {
-      axios
-        .get("/api/dashboard/chart", { params: { ID: 123 } })
-        .then(response => {
-          this.chartOption = {
-            title: {
-              text: "ECharts 入门示例"
-            },
-            tooltip: {},
-            xAxis: {
-              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-            },
-            yAxis: {},
-            series: [
-              {
-                name: "销量",
-                type: "bar",
-                data: response.data
-              }
-            ]
-          };
-        });
+      request({
+        url: "/api/dashboard/chart",
+        method: "get",
+        params: { ID: 123 }
+      }).then(response => {
+        this.chartOption = {
+          title: {
+            text: "ECharts 入门示例"
+          },
+          tooltip: {},
+          xAxis: {
+            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+          },
+          yAxis: {},
+          series: [
+            {
+              name: "销量",
+              type: "bar",
+              data: response.data
+            }
+          ]
+        };
+      });
     }
   },
   beforeDestroy() {
